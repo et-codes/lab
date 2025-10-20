6abf080e2c7786972e596b7cf6964ddf468753a6
et-codes <ericthornton43@gmail.com>
2025-10-19 18:57:39 -0600

    CSRF and post request

--- bookings/pkg/render/render.go
+++ bookings/pkg/render/render.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/et-codes/lab/bookings/pkg/config"
 	"github.com/et-codes/lab/bookings/pkg/models"
+	"github.com/justinas/nosurf"
 )
 
 var app *config.AppConfig
@@ -18,12 +19,14 @@ func NewTemplate(a *config.AppConfig) {
 }
 
 // AddDefaultData adds default info that is required on every template
-func AddDefaultData(td *models.TemplateData) *models.TemplateData {
+func AddDefaultData(td *models.TemplateData, r *http.Request) *models.TemplateData {
+	td.CSRFToken = nosurf.Token(r)
+
 	return td
 }
 
 // RenderTemplate renders templates using html/template
-func RenderTemplate(w http.ResponseWriter, tmpl string, td *models.TemplateData) {
+func RenderTemplate(w http.ResponseWriter, r *http.Request, tmpl string, td *models.TemplateData) {
 	var tc map[string]*template.Template
 
 	if app.UseCache {
@@ -41,7 +44,7 @@ func RenderTemplate(w http.ResponseWriter, tmpl string, td *models.TemplateData)
 
 	buf := new(bytes.Buffer)
 
-	td = AddDefaultData(td)
+	td = AddDefaultData(td, r)
 
 	// render the template
 	err := t.Execute(buf, td)
